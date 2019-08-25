43913f0b10450d57cb3de8a69ba1f313620fa562
Расуль Арсланов <rasularslanoov@gmail.com>
2019-08-25 22:44:50 +0500

    Fixed sidebar

--- resources/views/admin/_nav.blade.php
+++ resources/views/admin/_nav.blade.php
@@ -1,6 +1,6 @@
 <ul class="nav nav-tabs mb-3">
-    <li class="nav-item"><a class="nav-link active" href="{{ route('admin.home') }}">Dashboard</a></li>
-    <li class="nav-item"><a class="nav-link" href="{{ route('admin.users.index') }}">Users</a></li>
+    <li class="nav-item"><a class="nav-link {{ $page === '' ? ' active' : '' }}" href="{{ route('admin.home') }}">Dashboard</a></li>
+    <li class="nav-item"><a class="nav-link {{ $page === 'users' ? ' active' : '' }}" href="{{ route('admin.users.index') }}">Users</a></li>
 </ul>
 
 @foreach ($errors->all() as $error)
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -5,4 +5,6 @@
         <li class="nav-item"><a class="nav-link active" href="{{ route('admin.home') }}">Dashboard</a></li>
         <li class="nav-item"><a class="nav-link" href="{{ route('admin.users.index') }}">Users</a></li>
     </ul>
+
+    Hello!
 @endsection
