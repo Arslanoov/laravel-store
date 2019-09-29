cca6adf22f21ad28dbee41b2e35bc7f7c4c6722c
Расуль Арсланов <rasularslanoov@gmail.com>
2019-09-29 16:40:27 +0500

    Added newsletter emails migration

---
+++ database/migrations/2019_09_29_113620_create_newsletter_emails_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNewsletterEmailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('newsletter_emails', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('email', 255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('newsletter_emails');
+    }
+}
