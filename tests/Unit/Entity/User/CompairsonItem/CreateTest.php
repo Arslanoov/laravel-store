c01842e270d4426c10ff3910229d02e12c5664c5
Расуль Арсланов <rasularslanoov@gmail.com>
2019-09-30 15:35:51 +0500

    Added compairson item test

---
+++ tests/Unit/Entity/User/CompairsonItem/CreateTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Unit\Entity\User\CompairsonItem;
+
+use App\Entity\Shop\Product\Product;
+use App\Entity\User\User;
+use Tests\TestCase;
+
+class CreateTest extends TestCase
+{
+    public function testNew(): void
+    {
+        $user = factory(User::class)->make([
+            'id' => 1
+        ]);
+
+        $product = factory(Product::class)->make([
+            'id' => 1
+        ]);
+
+        $item = CompairsonItem::new(
+            $userId = $user->id,
+            $productId = $product->id
+        );
+
+        $this->assertEquals($item->user_id, $userId);
+        $this->assertEquals($item->product_id, $productId);
+    }
+}
